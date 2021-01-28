dfa1c5f324f3c749ca1c4e203dfae0c9be2a5469
EliasAlvial <Elias.Alvial@gmail.com>
2021-01-27 21:50:11 -0500

    planet2 has been added and is ready to merge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,5 +12,10 @@ public class Main {
 	troyo.Heat="56848";
 	troyo.temperature=32216545;
 	troyo.Radius="999899km";
-    }
+
+	Planet2 Omaro =new Planet2();
+	Omaro.color="blue";
+	Omaro.Radius="878893278937958km";
+
+	}
 }
